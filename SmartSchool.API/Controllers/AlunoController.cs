1f2608746604166e953e251ac5440ef69a6daf94
IlkaCarmo <ilka_2009@ig.com.br>
2021-12-13 22:53:18 -0300

    projeto iniciado

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+            }
+        };
+
+            public AlunoController() { }
+
+         [HttpGet]
+         public IActionResult Get()
+        {
+            return Ok("Alunos:Marta, Paula, Lucas,Rafa");
+        }
+        
+    }
+}
